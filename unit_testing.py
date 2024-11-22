cd21e846c12fba25a10cc87295ba2f4a958be79f
Behnke02 <72123302+Behnke02@users.noreply.github.com>
2024-11-21 23:53:52 -0700

    Initial Commit of Unit Testing Class
    Created an initial skeleton of required unit tests for program.  Includes tests for proper argument length in pull requests and authors, proper website response codes, and Github REST API Rate Limit checks.
    TODO: More involved testing, integration with main program.

---
+++ unit_testing.py
@@ -0,0 +1,22 @@
+import unittest
+import repos_module
+import git_scrape_gui
+
+class TestSuite(unittest.TestCase):
+    def test_num_repos(self):
+        pass
+
+    def test_repo_instance(self):
+        self.assertIsInstance(self, repos_module.GitHubRepository)
+
+    def test_status_code(self, response_code):
+        self.assertEqual(response_code, 200, msg=f"Error status code, impossible to access page ಠ_ಠ: {response_code}")
+
+    def check_rate_limit(self, rate_remaining):
+        self.assertGreater(rate_remaining, 0, msg="Rate Limit Exceeded, try again later.")
+
+    def check_pull_request_args(self, **kwargs):
+        self.assertGreaterEqual(len(kwargs), 11, "Not enough information found in keyword arguments.")
+
+    def check_author_args(self, **kwargs):
+        self.assertGreaterEqual(len(kwargs), 5, "Not enough information found in keyword arguments.")
